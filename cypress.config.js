ba5b66913573933da8cf78bf2650cb35c83805cf
Juan R. Ovalle <Juanrovalle@gmail.com>
2024-06-12 18:52:16 -0400

    fix intentation

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    video: false,
-    e2e: {
-        baseUrl: 'http://localhost:5000',
-    },
+  video: false,
+  e2e: { baseUrl: 'http://localhost:5000' }
 })
