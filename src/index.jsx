524e82589817e67116f1ffeef23916300f02a7a4
Juan R. Ovalle <Juanrovalle@gmail.com>
2024-06-12 23:39:06 -0400

    Old version of react dom

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-// ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
